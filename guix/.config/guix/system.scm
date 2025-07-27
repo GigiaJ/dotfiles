214c64f1e40e81e9d5a7da1bc32c0ec18130559c
Gigiaj <gigiaboone@yahoo.com>
2025-07-26 21:25:35 -0500

    remove comments

--- guix/.config/guix/system.scm
+++ guix/.config/guix/system.scm
@@ -3,10 +3,8 @@
       (add-to-load-path (dirname f))
       (display "Warning: current-filename returned #f\n")))
 
-;; Now you can import your custom module without error.
 (use-modules (utils))
 (use-modules (base-system))
-;; The rest of your logic remains the same.
 (if (apple-hardware?)
     (load "macbook-system.scm")
     (load "desktop-system.scm"))
