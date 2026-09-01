2210222676a536e6df752275752c9b3c63d86326
GigiaJ <gigiaboone@yahoo.com>
2026-09-01 18:03:16 -0500

    remove ovmf

--- guix/.config/guix/home.scm
+++ guix/.config/guix/home.scm
@@ -50,7 +50,6 @@
                               "tree"
                               "emacs-eaf-browser"
                               "swtpm"
-                              "ovmf"
                               )))
 
 
